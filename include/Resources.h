0de9eb65987b88fa9eb52e4327ff5878da8364c9
V?in? Ala-H?rk?nen <vae@iki.fi>
2014-01-19 21:52:52 +0200

    Resource definitions for OGG files

--- include/Resources.h
+++ include/Resources.h
@@ -1,2 +1,37 @@
 #pragma once
 #include "cinder/CinderResources.h"
+
+#define RES_00_00 CINDER_RESOURCE( ../resources/, 00_00.ogg, 128, OGG )
+#define RES_00_01 CINDER_RESOURCE( ../resources/, 00_01.ogg, 129, OGG )
+#define RES_00_02 CINDER_RESOURCE( ../resources/, 00_02.ogg, 130, OGG )
+
+#define RES_01_01 CINDER_RESOURCE( ../resources/, 01_01.ogg, 132, OGG )
+#define RES_01_02 CINDER_RESOURCE( ../resources/, 01_02.ogg, 133, OGG )
+#define RES_01_03 CINDER_RESOURCE( ../resources/, 01_03.ogg, 134, OGG )
+#define RES_01_04 CINDER_RESOURCE( ../resources/, 01_04.ogg, 135, OGG )
+#define RES_01_u1 CINDER_RESOURCE( ../resources/, 01_u1.ogg, 136, OGG )
+
+#define RES_02_01 CINDER_RESOURCE( ../resources/, 02_01.ogg, 137, OGG )
+#define RES_02_02 CINDER_RESOURCE( ../resources/, 02_02.ogg, 138, OGG )
+#define RES_02_03 CINDER_RESOURCE( ../resources/, 02_03.ogg, 139, OGG )
+#define RES_02_04 CINDER_RESOURCE( ../resources/, 02_04.ogg, 140, OGG )
+#define RES_02_u1 CINDER_RESOURCE( ../resources/, 02_u1.ogg, 141, OGG )
+
+#define RES_03_01 CINDER_RESOURCE( ../resources/, 03_01.ogg, 142, OGG )
+#define RES_03_02 CINDER_RESOURCE( ../resources/, 03_02.ogg, 143, OGG )
+#define RES_03_03 CINDER_RESOURCE( ../resources/, 03_03.ogg, 144, OGG )
+#define RES_03_04 CINDER_RESOURCE( ../resources/, 03_04.ogg, 145, OGG )
+#define RES_03_u1 CINDER_RESOURCE( ../resources/, 03_u1.ogg, 146, OGG )
+
+#define RES_04_01 CINDER_RESOURCE( ../resources/, 04_01.ogg, 147, OGG )
+#define RES_04_02 CINDER_RESOURCE( ../resources/, 04_02.ogg, 148, OGG )
+#define RES_04_03 CINDER_RESOURCE( ../resources/, 04_03.ogg, 149, OGG )
+#define RES_04_04 CINDER_RESOURCE( ../resources/, 04_04.ogg, 150, OGG )
+#define RES_04_05 CINDER_RESOURCE( ../resources/, 04_05.ogg, 151, OGG )
+#define RES_04_u1 CINDER_RESOURCE( ../resources/, 04_u1.ogg, 152, OGG )
+
+#define RES_99_01 CINDER_RESOURCE( ../resources/, 99_01.ogg, 153, OGG )
+#define RES_99_02 CINDER_RESOURCE( ../resources/, 99_02.ogg, 154, OGG )
+#define RES_99_03 CINDER_RESOURCE( ../resources/, 99_03.ogg, 155, OGG )
+#define RES_99_04 CINDER_RESOURCE( ../resources/, 99_04.ogg, 156, OGG )
+#define RES_99_99 CINDER_RESOURCE( ../resources/, 99_99.ogg, 157, OGG )
